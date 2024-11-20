d598f0180a64abe7a4af94ab7d95138ae19cdb43
AleksanderBaran <aleksanderbaran600@gmail.com>
2024-11-20 18:46:56 +0100

    Update Ship.cc

--- aleksanderBaran/Ship.cc
+++ aleksanderBaran/Ship.cc
@@ -1,7 +1,9 @@
 #include "Ship.h"
+#include "Board.h"
 #include <iostream>
 using namespace std;
-Ship::Ship() : length(0), hits(0) {} //The same thing as the previous
+
+Ship::Ship() : length(0), hits(0) {}
 
 void Ship::setShip(int len) 
 {
@@ -10,5 +12,13 @@ void Ship::setShip(int len)
 }
 bool Ship::isSunk() 
 {
-    return hits == length;
+    return hits >= length;
 }
+void Ship::hit()
+{
+    hits++;
+}
+
+
+
+
