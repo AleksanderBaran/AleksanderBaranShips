ed818b573ed3dafede156b430c7eea576c3692db
AleksanderBaran <aleksanderbaran600@gmail.com>
2024-11-20 18:47:06 +0100

    Update Ship.h

--- aleksanderBaran/Ship.h
+++ aleksanderBaran/Ship.h
@@ -1,14 +1,15 @@
 #ifndef SHIP_H
 #define SHIP_H
 #include <iostream>
-class Ship {
+class Ship 
+{
 public:
     int length;
     int hits;
-
     Ship();
     void setShip(int len);
     bool isSunk();
+    void hit();
 };
 
 #endif
